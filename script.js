291a585b483d98fb1726d5df5cb778b66910fb3d
JustinBrilliantThendri <justinvirgo274@gmail.com>
2023-06-22 20:27:45 +0700

    Update

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let qrcode = document.querySelector(".qrcode");
-let content = document.querySelector(".content");
+/// <reference path="typings/globals/jquery/index.d.ts"/>
 
-function generate(){
-    if(content.value != ""){
-        let api = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${content.value}`;
-        qrcode.src = api;
-    }else{
-        qrcode.src = "";
-    }
-}
+let api = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+let loading = "<span class='loading'></span>";
+
+$(() => {
+    $(".container .btn").click(() => {
+        $(".container .qrcode").empty();
+        if($(".container .content").val() != ""){
+            $(".container .qrcode").css("display", "flex");
+            $(".container .qrcode").append(loading, `<img class='output' src='${api + $(".container .content").val()}'>`);
+            $(".container .qrcode .output").on("load", () => {
+                $(".container .qrcode .output").show();
+                $(".container .qrcode .loading").hide();
+            });
+        }else{
+            $(".container .qrcode").css("display", "none");
+        }
+    });
+});
